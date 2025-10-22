e1ec3087fb9c725df0f4e8902182af5350acc2b0
Sounak Nandi <sounaknandi24@gmail.com>
2025-10-22 22:00:09 +0530

    src files

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# Empty — just to mark the folder as a package:
+# It is required to make Python treat the directories as 
+# containing packages; this is done to prevent directories 
+# with a common name, such as 'string', from unintentionally 
+# hiding valid modules that occur later on the module search path.
+
+
+
+# Basically this empty file tells Python that 
+# any files under this directory should be treated as a package.
